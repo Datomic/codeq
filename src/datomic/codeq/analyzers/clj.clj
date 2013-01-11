2b657d190805db73ef153f356317fc9327bf83b6
Bobby Calderwood <bobby_calderwood@mac.com>
2013-01-11 16:27:31 -0500

    Add Contributors to the copyright header

--- src/datomic/codeq/analyzers/clj.clj
+++ src/datomic/codeq/analyzers/clj.clj
@@ -1,4 +1,4 @@
-;;   Copyright (c) Metadata Partners, LLC. All rights reserved.
+;;   Copyright (c) Metadata Partners, LLC and Contributors. All rights reserved.
 ;;   The use and distribution terms for this software are covered by the
 ;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ;;   which can be found in the file epl-v10.html at the root of this distribution.
