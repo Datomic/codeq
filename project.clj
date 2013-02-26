0515f50143474ff1440a5ceffbeaf5cf03c9b56c
Bobby Calderwood <bobby_calderwood@mac.com>
2013-02-26 14:59:06 -0500

    Update Clojure to 1.5.0-RC17 and Datomic to 0.8.3826

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main datomic.codeq.core
   :plugins [[lein-tar "1.1.0"]]
-  :dependencies [[com.datomic/datomic-free "0.8.3814"]
+  :dependencies [[com.datomic/datomic-free "0.8.3826"]
                  [commons-codec "1.7"]
-                 [org.clojure/clojure "1.5.0-RC16"]]
+                 [org.clojure/clojure "1.5.0-RC17"]]
   :source-paths ["src" "examples/src"])
